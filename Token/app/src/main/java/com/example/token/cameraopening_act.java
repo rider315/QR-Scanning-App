fa69f59fec7f6704c8cef3d8c1c48629dcd0d503
myusername <jaatsaab735@gmail.com>
2023-04-04 23:28:12 +0530

    this is my first and final commit to this repo

---
+++ Token/app/src/main/java/com/example/token/cameraopening_act.java
@@ -0,0 +1,24 @@
+package com.example.token;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class cameraopening_act extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cameraopening);
+
+
+
+
+
+
+    }
+}
